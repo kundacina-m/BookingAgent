958d290e33db08ace33ea1fd4125f79375b86b78
Milan Kundacina <milankundacina@uns.ac.rs>
2019-06-30 03:00:31 +0200

    Some nice UI changes, need some more work on Room details

--- app/src/main/java/com/example/bookingagent/utils/Constants.kt
+++ app/src/main/java/com/example/bookingagent/utils/Constants.kt
@@ -4,19 +4,27 @@ const val BASE_URL = "http://192.168.0.103:8080/service/"
 
 const val FILE_CHOOSER_IMAGE = 1
 
+object Cache {
+
+    var pass = ""
+
+    fun cachePass(password: String) {
+        pass = password
+    }
+}
 
 object ApiHeaders {
 
-	val map = hashMapOf(
-		Pair("Authorization", ""),
-		Pair("Content-Type", "text/xml;charset=utf-8")
-	)
+    val map = hashMapOf(
+        Pair("Authorization", ""),
+        Pair("Content-Type", "text/xml;charset=utf-8")
+    )
 
-	fun addToken(token: String) {
-		map["Authorization"] = token
-	}
+    fun addToken(token: String) {
+        map["Authorization"] = token
+    }
 
-	fun removeToken() {
-		map["Authorization"] = ""
-	}
+    fun removeToken() {
+        map["Authorization"] = ""
+    }
 }
